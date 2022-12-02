2d68741ed44ef9e5d4e1fa8558b5005dc3d8c5c2
dmitry <rbdeman@yandex.ru>
2022-12-02 15:42:48 +0300

    промежуточное сохранение

--- Zadacha_two/Program.cs
+++ Zadacha_two/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+Console.Clear();
+
+
+int DataEntry(string str)
+{
+    Console.WriteLine(str);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int FindSum(int num)
+{
+    int sum = 0;
+    for (int i = num; num > 0; i = num / 10)
+    {
+        sum = sum + num % 10;
+    }
+    return sum;
+}
+
+
+int number = DataEntry("Введи число: ");
+int sum = FindSum(number);
+Console.WriteLine("сумма цифр в числе: " + sum);
