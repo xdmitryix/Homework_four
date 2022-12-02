a24abeac7a3c832d24b750aeae972f05b686e3be
dmitry <rbdeman@yandex.ru>
2022-12-02 15:22:08 +0300

    сделал первую задачу

--- Zadacha_one/Program.cs
+++ Zadacha_one/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Clear();
+
+int DataEntry(string str)
+{
+    Console.WriteLine(str);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int FindResult(int numOne, int numTwo)
+{
+    int result = numOne;
+    for (int i = 2; i <= numTwo; i++)
+    {
+        result=result*numOne;
+    }
+    return result;
+}
+
+int numberOne = DataEntry("Введи число A: ");
+int numberTwo = DataEntry("Введи число B: ");
+int result = FindResult(numberOne, numberTwo);
+Console.WriteLine("результат: " + result);
