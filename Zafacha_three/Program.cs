b767668dd0285149d1e1fa7eb2e1cc4500d38e6c
dmitry <rbdeman@yandex.ru>
2022-12-04 12:33:36 +0300

    Сделал третью задачу

--- Zafacha_three/Program.cs
+++ Zafacha_three/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+
+Console.Clear()
+int[] numbers = new int[8];
+for (int i=0; i< numbers.Length; i++)
+{
+    numbers[i] = Random.Next(0, 99);
+    Console.WriteLine(numbers[i]);
+}
