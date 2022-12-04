343b7b56c5bb9fae10c7331c51432909570ccd9d
dmitry <rbdeman@yandex.ru>
2022-12-04 21:37:01 +0300

    добавил вариант с функцией

--- Zafacha_three/Program.cs
+++ Zafacha_three/Program.cs
@@ -14,11 +14,43 @@ for (int i = 0; i < numbers.Length; i++)
     }
     else
         if (i == 7)
-        {
-            Console.Write(numbers[i] + "]");
-        }
-        else
-        {
-            Console.Write(numbers[i] + ",");
-        }
+    {
+        Console.Write(numbers[i] + "]");
+    }
+    else
+    {
+        Console.Write(numbers[i] + ",");
+    }
 }
+
+
+//  Ниже добавил вариант с функцией. Но он у меня не работает. Что-то сделал не так, а что именно-понять не могу. Напишите в комментарии как
+//  мне правильно сделать функцию для моего решения. Спасибо !. 
+
+
+
+// Console.Clear();
+
+// void result(int[] numbers)
+// {
+//     for (int i = 0; i < numbers.Length; i++)
+//     {
+//         numbers[i] = new Random().Next(0, 99);
+//         if (i == 0)
+//         {
+//             Console.Write("[" + numbers[i] + ",");
+//         }
+//         else
+//             if (i == 7)
+//         {
+//             Console.Write(numbers[i] + "]");
+//         }
+//         else
+//         {
+//             Console.Write(numbers[i] + ",");
+//         }
+//     }
+// }
+
+// int arr = result(new int[8]);
+// Console.WriteLine(arr);
